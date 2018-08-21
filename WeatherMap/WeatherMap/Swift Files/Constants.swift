e512a8ee19c4388b4c5d0c7ddc8cf172e1683a5d
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2018-08-21 20:09:46 +0530

    Adding SwiftyJSON

---
+++ WeatherMap/WeatherMap/Swift Files/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  WeatherMap
+//
+//  Created by Aditya Vikram Godawat on 21/08/18.
+//  Copyright © 2018 Aditya Vikram Godawat. All rights reserved.
+//
+
+let baseUrl = "http://api.openweathermap.org/data/2.5/weather"
